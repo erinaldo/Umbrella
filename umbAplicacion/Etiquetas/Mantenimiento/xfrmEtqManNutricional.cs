660e124de001ef094c208672e59a780931b321a6
more2286 <moremoreno286@hotmail.com>
2019-08-25 19:07:05 -0500

    Prueba de mantenimiento de etiqueta

--- umbAplicacion/Etiquetas/Mantenimiento/xfrmEtqManNutricional.cs
+++ umbAplicacion/Etiquetas/Mantenimiento/xfrmEtqManNutricional.cs
@@ -10,9 +10,12 @@ namespace umbAplicacion.Etiquetas.Mantenimiento
 {
     public partial class xfrmEtqManNutricional : umbAplicacion.Plantillas.Mantenimiento.xfrmPlaMantenimiento
     {
-        public xfrmEtqManNutricional()
-        {
+        public xfrmEtqManNutricional() { InitializeComponent(); }
+        public xfrmEtqManNutricional(int varFormulario, int varOperacion, int varRegistro) {
             InitializeComponent();
+            varForCodigo = varFormulario;
+            varOpeCodigo = varOperacion;
+            varRegCodigo = varRegistro;
         }
     }
 }
